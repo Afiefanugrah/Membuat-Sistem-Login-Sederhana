5ac6a0fb928d83dfa9f18fe7d85f5c47c3659b09
M. Afief Anugrah <mafiefanugrah01@gmail.com>
2024-09-12 20:44:36 +0700

    ulang

--- db.config.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize')
-
-const sequelize = new Sequelize('db_login', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
-module.exports = sequelize
