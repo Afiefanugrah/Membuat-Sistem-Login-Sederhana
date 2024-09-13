bd1e921d91747158548ebcda30d4eb8cbd318ad2
M. Afief Anugrah <mafiefanugrah01@gmail.com>
2024-09-14 01:55:02 +0700

    register

--- server.js
+++ server.js
@@ -3,10 +3,31 @@ const port = 3200
 
 const app = express()
 
+app.use(express.json())
+
+let usersData = []
+
 
 app.get('/', function (req, res) {
     res.json({
         data: "halo",
+        metadata: "halaman awal"
+    })
+})
+
+app.post('/', function (req, res) {
+    const { username, password } = req.body
+    console.log('Request body:', req.body); // Debugging
+
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Username atau password tidak ada!' });
+    }
+
+    // Simpan data pengguna ke dalam array
+    usersData.push({ username, password });
+    res.json({
+        data: username,
+        usersData: usersData,
         metadata: "halaman awal"
     })
 })
